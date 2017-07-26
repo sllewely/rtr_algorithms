a0f85dca1c13820f05ec65035a2ca37acb5ff791
Sarah Llewelyn <sllewelyn@renttherunway.com>
2017-07-26 18:50:54 -0400

    white space

--- algorithms/003_stack/stack.rb
+++ algorithms/003_stack/stack.rb
@@ -1,34 +1,34 @@
 # sllewelyn
 
 class Stack
-	def push(val)
-		node = Node.new(val)
-		if is_empty?
-			@last = node
-		else
-			node.prev = @last
-			@last = node
-		end
-	end
+  def push(val)
+    node = Node.new(val)
+    if is_empty?
+      @last = node
+    else
+      node.prev = @last
+      @last = node
+    end
+  end
 
-	def pop
-		return if is_empty?
-		output = @last
-		@last = @last.prev
-		output.val
-	end
+  def pop
+    return if is_empty?
+    output = @last
+    @last = @last.prev
+    output.val
+  end
 
-	def is_empty?
-		@last.nil?
-	end
+  def is_empty?
+    @last.nil?
+  end
 end
 
 class Node
-	attr_accessor :prev, :val
+  attr_accessor :prev, :val
 
-	def initialize(val)
-		@val = val
-	end
+  def initialize(val)
+    @val = val
+  end
 end
 
 
@@ -39,8 +39,8 @@ str = 'alphabet'
 rstr = ''
 str.chars.each { |c| stack.push(c) }
 while !stack.is_empty?
-	rstr = stack.pop
-	puts rstr
+  rstr = stack.pop
+  puts rstr
 end
 
 puts rstr

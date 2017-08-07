c1d4571affde5a7c6efb9e33d6be71503e7a44c7
Mike Boyle <mikesboyle@gmail.com>
2017-08-06 22:51:13 -0400

    additional logging for towers recursion

--- algorithms/003_stack/towers.py
+++ algorithms/003_stack/towers.py
@@ -4,6 +4,7 @@ def towers():
     # set up pegs and rings
     for ring in reversed(xrange(1, num_rings + 1)):
         p1.append(ring)
+    print("starting position")
     print_status()
 
     # start by trying to move the bottom ring
@@ -11,14 +12,18 @@ def towers():
     print_move_count()
 
 def move_ring(ring, source, dest, other):
+    print("Trying to move ring {} from {} to {}...".format(ring, peg_to_s(source), peg_to_s(dest)))
     # base case: if ring is at top of stack, move it
     if ring == source[-1]:
         dest.append(source.pop()) # actually move the ring
 
         # keep track of number of moves and print what happened
         count_move(ring)
+        print ("Success! Finished moving ring {} from {} to {}.".format(ring, peg_to_s(source), peg_to_s(dest)))
         print_status()
+
     else:
+        print ("Ring {} can't be moved yet...".format(ring))
         next_ring = next_ring_above(ring, source)
         # try to move the next ring up in the stack to the "other" peg
         move_ring(next_ring, source, other, dest)
@@ -26,17 +31,17 @@ def move_ring(ring, source, dest, other):
         move_ring(ring, source, dest, other)
         # then move the next ring up on top of the current ring
         move_ring(next_ring, other, dest, source)
+        print ("Finished moving ring {} from {} to {}...".format(ring, peg_to_s(source), peg_to_s(dest)))
 
 def next_ring_above(ring, source):
     next_index = source.index(ring) + 1
     return source[next_index]
 
 def count_move(ring):
-    key = str(ring)
-    if key in move_log:
-        move_log[key] += 1
+    if ring in move_log:
+        move_log[ring] += 1
     else:
-        move_log[key] = 1
+        move_log[ring] = 1
 
 def print_move_count():
     for ring, num_moves in sorted(move_log.iteritems()):
@@ -56,6 +61,16 @@ def ring_to_s(peg, index):
         return peg[index]
     except IndexError:
         return "|"
+
+def peg_to_s(peg):
+    if id(peg) == id(p1):
+        return "peg 1"
+    elif id(peg) == id(p2):
+        return "peg 2"
+    elif id(peg) == id(p3):
+        return "peg 3"
+    else:
+        return "oops"
 
 if __name__ == "__main__":
     move_log = {}
